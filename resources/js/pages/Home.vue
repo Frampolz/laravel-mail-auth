4f798ef8a0ff4795e3b12a03837b2861436a1305
Frampolz <francesco.colloca@hotmail.it>
2022-03-17 16:54:41 +0100

    add repo github

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,10 +1,37 @@
 <template>
-    <div>home</div>
+    <div>
+        <Repo :repoArray="repo" />
+    </div>
 </template>
 
 <script>
+import Repo from "./Repo.vue";
 export default {
     name: "Home",
+    components: {
+        Repo,
+    },
+    data() {
+        return {
+            repo: null,
+        };
+    },
+    created() {
+        this.getRepo();
+    },
+    methods: {
+        getRepo() {
+            axios
+                .get("https://api.github.com/users/frampolz/repos")
+                .then((result) => {
+                    console.log(result);
+                    this.repo = result.data;
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+    },
 };
 </script>
 
---
+++ resources/js/pages/Repo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div v-for="(repo, index) in repoArray" :key="index" class="repo">
+            <div class="repo-title">{{ repo.name }}</div>
+            <div class="repo-language">{{ repo.language }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Repo",
+    data() {
+        return {};
+    },
+    props: {
+        repoArray: {
+            type: Array,
+            default() {
+                return [];
+            },
+        },
+    },
+};
+</script>
+
+<style></style>
